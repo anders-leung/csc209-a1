09fd6044ad434b05b3b3c96682a83368c1f922af
c3leungb <c3leungb@859cd7a6-6295-4d08-baa5-4d9a185f5c11>
2015-01-29 16:08:46 +0000

    Added a bugged version of addecho.c

--- a1/addecho.c
+++ a1/addecho.c
@@ -1,41 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char ** argv) {
 	int delay = 8000;
 	int volume_scale = 4;
-	FILE *input, *output;
+	int source_index = 1;
+	int dest_index = 2;
+	FILE *input, *input2, *output;
 	
 	if (argc == 3 || argc == 5 || argc == 7) {
-		if (argc == 5) {
-			if (strcmp(argv[1], "-d") == 0) {
-				delay = argv[2];
-			} else {
-				volume_scale = argv[2];
+		int opt;
+		while ((opt = getopt(argc, argv, "d:v:")) != -1) {
+			printf("opt = %d\n", opt);
+			switch (opt) {
+			case 'd':
+				delay = atoi(optarg);
+				break;
+			case 'v':
+				volume_scale = atoi(optarg);
+				break;
+			default:
+				fprintf(stderr, "Usage: %s [-d delay] \
+					[-v volume_scale] sourvewav \
+					destwav\n", argv[0]);
+				exit(1);
 			}
 		}
-
-		else if (argc == 7) {
-			delay = argv[2];
-			volume_scale = argv[4];
+		if (optind >= argc) {
+			fprintf(stderr, "Expected argument after options\n");
+			exit(1);
 		}
-	
-		else {
-			input = fopen(argv[-2], "r");
-			output = fopen(argv[-1], "w+");
-			char header[44 + (delay * 4)];
-			fread(header, 1, 4, input);
-			int integer;
-			fread(&integer, sizeof(integer), 1, input);
-			integer = integer + (delay * 2);
-			fwrite(&integer, sizeof(integer), 1, header);
-			fread(header, 1, 32, input);
-			fread(&integer, sizeof(integer), 1, input);
-			integer = integer + (delay * 2);
-			fwrite(&integer, sizeof(integer), 1, header);
-			fwrite(header, sizeof(header), 1, output);
-			return 0;
+		printf("i got past the cases");
+		input = fopen(argv[source_index], "r");
+		fseek(input, 0, SEEK_END);
+		long size = ftell(input);
+		fseek(input, 0, SEEK_SET);
+		input2 = fopen(argv[source_index], "r");
+		output = fopen(argv[dest_index], "w+");
+		char header[44];
+		size_t bytes;
+		bytes = fread(header, 1, 4, input);
+		fwrite(header, 1, 4, output);
+		int integer;
+		fread(&integer, sizeof(integer), 1, input);
+		integer = integer + (delay * 2);
+		fwrite(&integer, sizeof(integer), 1, output);
+		bytes = fread(header, 1, 32, input);
+		fwrite(header, 1, bytes, output);
+		fread(&integer, sizeof(integer), 1, input);
+		integer = integer + (delay * 2);
+		fwrite(&integer, sizeof(integer), 1, output);
+		fseek(input2, 44, 0);
+		short orig[delay], echo[delay];
+		bytes = fread(orig, sizeof(short), delay, input);
+		fwrite(orig, sizeof(short), bytes, output);
+		long current = 44 + (delay * 2);
+		int i;
+		while (!feof(input) && !ferror(input)) {
+			if ((size - current) > delay * 2) {
+				fread(orig, sizeof(short), delay, input);
+				fread(echo, sizeof(short), delay, input2);
+				for (i = 0; i < delay; i++) {
+					echo[i] = echo[i] / volume_scale;
+					echo[i] = echo[i] + orig[i];
+				}
+				fwrite(echo, sizeof(short), delay, output);
+				current = current + delay * 2;
+			} else {
+				bytes = fread(orig, sizeof(short), ((size - current) / 2), input);
+				fread(echo, sizeof(short), ((size - current) / 2), input2);
+				for (i = 0; i < ((size - current) / 2); i++) {
+					echo[i] = echo[i] / volume_scale;
+					echo[i] = echo[i] + orig[i];
+				}
+				fwrite(echo, sizeof(short), ((size - current) / 2), output);
+				fread(orig, 1, 1, input);
+			}
 		}
+		int zero_samples = delay - ((size - 44) / 2);
+		int non_header_samples = ((size - 44) / 2);
+		printf("non_header_samples = %d\n", non_header_samples);
+		printf("zero_samples = %d\n", zero_samples);
+		if (zero_samples > 0) {
+			for (i = 0; i < zero_samples; i++) {
+				short zero = 0;
+				fwrite(&zero, sizeof(short), 1, output);
+			}
+		}
+		fread(echo, sizeof(short), delay, input2);
+		for (i = 0; i < delay; i++) {
+			echo[i] = echo[i] / volume_scale;
+		}
+		fwrite(echo, sizeof(short), delay, output);
+		fclose(input);
+		fclose(input2);
+		fclose(output);
 	}
 
 	else {
@@ -44,3 +104,5 @@ int main(int argc, char ** argv) {
 	}
 
 	return 0;
+}
+
